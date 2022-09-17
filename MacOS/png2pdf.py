1410ef548cea8d3ebdc983a2ec2b0bb76dd7544f
habi <habijung0@gmail.com>
2022-09-17 16:28:52 +0900

    Update MacOS Utility
    - Mouse Click Swap
    - png2pdf

---
+++ MacOS/png2pdf.py
@@ -0,0 +1,71 @@
+import os
+import argparse
+import cv2
+import img2pdf
+
+
+# Argument 관리
+parser = argparse.ArgumentParser(description="PNG to PDF")
+parser.add_argument("left", type=int, default=0, help="여백이 끝나는 x 좌표")
+parser.add_argument("top", type=int, default=0, help="여백이 끝나는 y 좌표")
+parser.add_argument(
+    "-c", "--crop", default=False, action="store_true", help="단일 이미지 자르기 테스트"
+)
+parser.add_argument("-f", "--file", type=str, help="자르기 테스트에 사용할 파일명")
+args = parser.parse_args()
+print(args)
+
+
+# Path 관리
+dirname = os.path.dirname(__file__) + "/"
+result = "result.pdf"
+
+
+# 단일 파일 crop 테스트
+if args.crop is True:
+    print("단일 파일 crop 테스트")
+
+    if args.file is None:
+        print("Error: 추가된 파일이 없음")
+
+    elif ".png" not in args.file:
+        print("Error: PNG 파일이 아님")
+
+    else:
+        print("단일 파일 Crop 실행")
+        print(f"파일명: {dirname + args.file}")
+
+        image = cv2.imread(dirname + args.file)
+
+        x, y = args.left, args.top
+        h, w, c = image.shape
+        image_crop = image[y : (h - y), x : (w - x)]
+
+        cv2.imshow("Cropped Result", image_crop)
+        cv2.waitKey(0)
+
+# Convert PNG to PDF
+else:
+    print("png2pdf 실행")
+
+    images = [file for file in sorted(os.listdir(dirname)) if ".png" in file]
+    images_read = [cv2.imread(img) for img in images]
+
+    x, y = args.left, args.top
+    h, w, c = images_read[0].shape
+    images_crop = [img[y : (h - y), x : (w - x)] for img in images_read]
+
+    for i in range(len(images)):
+        filename = "crop-" + images[i]
+        img = images_crop[i]
+        cv2.imwrite(dirname + filename, img)
+
+    images_crop = [file for file in sorted(os.listdir(dirname)) if "crop-" in file]
+
+    with open(result, "wb") as f:
+        f.write(img2pdf.convert(images_crop))
+
+    for file in images_crop:
+        os.remove(file)
+
+    print("png2pdf 종료")
